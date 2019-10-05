8a85b69692411e27ebcb39b1d7d5865b2daab967
efscomplex <efscomplex@gmail.com>
2019-10-05 13:46:45 +0200

    init react app

---
+++ src/components/title.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Title extends React.Component{
+    render(){
+        return(
+            <h1> Awesome Title from the React-Title-Component </h1>
+        )
+    }
+}
